da80a3f43ca8a43e5dbf883c734a35fe3b2d463f
Davide Figuccia <d.figuccia@eu.aswatson.com>
2025-05-09 20:08:14 +0200

    v0.1.3 - vote page

--- src/api/votesApi.js
+++ src/api/votesApi.js
@@ -1,10 +1,38 @@
 const VOTES_BASE = process.env.REACT_APP_API_BASE+"/votes";
 
 // Funzione per registrare una votazione
+export const submitVotes = async (token, categoryCode, votes) => {
+    const response = await fetch(`${VOTES_BASE}/submit/${categoryCode}`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ votes: votes })
+    });
 
+    if (!response.ok) {
+        throw new Error('Errore nella votazione');
+    }
+    return await response.json(); // restituisce un messaggio di successo
+};
 
 // Funzione per ottenere le nazioni che è possibile votare data una categoria
+export const getAvailableCountries = async (token, categoryCode) => {
+    const response = await fetch(`${VOTES_BASE}/available-countries/${categoryCode}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    });
 
+    if (!response.ok) {
+        throw new Error('Errore nel recuperare le nazioni disponibili');
+    }
+
+    return await response.json();
+}
 
 // Funzione per ottenere la categoria su cui sono aperte le votazioni
 export const getOpenCategory = async (token) => {
@@ -19,6 +47,8 @@ export const getOpenCategory = async (token) => {
     if (!response.ok) {
         if (response.status === 404) {
             throw new Error("NO_CATEGORY");
+        } else if(response.status === 400) {
+            throw new Error("ALREADY_VOTED");
         }
         throw new Error('Errore nel recuperare la categoria di voto');
     }
--- src/pages/VotePage.jsx
+++ src/pages/VotePage.jsx
@@ -1,35 +1,93 @@
-import {useEffect, useState} from "react";
-import {getOpenCategory} from "../api/votesApi";
-import {Box, Container, Typography} from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {getAvailableCountries, getOpenCategory, submitVotes} from "../api/votesApi";
+import {
+    Alert,
+    Box,
+    Button,
+    Container,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    Snackbar,
+    Typography
+} from "@mui/material";
 import useAuthStore from "../store/authStore";
 import {useNavigate} from "react-router-dom";
+import {getFlagEmoji} from "../utils/flagUtils";
 
 const VotePage = () => {
     const navigate = useNavigate();
     const {token, isAdmin} = useAuthStore();
-    const [category, setCategory] = useState('');
+    const [category, setCategory] = useState(null);
     const [noCategory, setNoCategory] = useState(false);
+    const [alreadyVoted, setAlreadyVoted] = useState(false);
+    const [availableCountries, setAvailableCountries] = useState([]);
+    const [votes, setVotes] = useState([]);
+    const [error, setError] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+    const [triggerFetchCategory, setTriggerFetchCategory] = useState(false);
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
 
     useEffect(() => {
-        if (isAdmin) {
-            navigate('/admin');
-        }
-
-        const fetchOpenCategory = async () => {
+        const fetchCategory = async () => {
             try {
                 const data = await getOpenCategory(token);
                 setCategory(data);
+                setNoCategory(false);
+                setAlreadyVoted(false);
+                setError('');
+                setSuccessMessage('');
+                setVotes([]);
             } catch (err) {
-                if (err.message == 'NO_CATEGORY') {
+                if (err.message === 'NO_CATEGORY') {
                     setNoCategory(true);
+                } else if (err.message === 'ALREADY_VOTED') {
+                    setAlreadyVoted(true);
+                    setNoCategory(false);
+                    setCategory({name: err.message, label: "Hai già votato!"});
+                    setVotes([])
+                    setCategory(null)
                 } else {
                     navigate('/logout');
                 }
             }
         };
-        fetchOpenCategory();
-    }, []);
+        fetchCategory();
+    }, [navigate, token, triggerFetchCategory]);
 
+    useEffect(() => {
+        if (category && category.votePoints && category.votePoints.length > 0) {
+            getAvailableCountries(token, category.name)
+                .then(data => {
+                    setAvailableCountries(data.countries);
+                }).catch(err => {
+                    setAvailableCountries([]);
+                    setError("Errore nel recupero delle nazioni disponibili!");
+                });
+        }
+    },[category, token]);
+
+    const isVoteValid = () => {
+        return (
+            votes.length === category.votePoints.length &&
+            votes.every(v => v.country && v.country !== '')
+        );
+    };
+
+    const onSubmit = async () => {
+        try {
+            await submitVotes(token, category.name, votes);
+            setCategory(null)
+            setAlreadyVoted(true);
+            setVotes([]); // reset selezioni
+            setSuccessMessage("Votazione inviata con successo! 🎉");
+            setError('')
+        } catch (err) {
+            setError("Votazione fallita!");
+            setSuccessMessage('');
+        }
+    };
 
     return (
         <Box
@@ -41,17 +99,124 @@ const VotePage = () => {
             <Container maxWidth="sm">
                 <Box mt={8} p={4} boxShadow={3} borderRadius={2}>
                     {
-                        noCategory && (
+                        alreadyVoted && (
                             <>
-                                <Typography noCategory={false} variant="h4" align="center" gutterBottom>Hey, so che non
-                                    vedi l'ora ma le votazioni non
-                                    sono ancora aperte!</Typography>
-                                <Typography noCategory={true} variant="h4" align="center" gutterBottom>Torna sabato 17
-                                    maggio 2025 alle 18:00
-                                    😉</Typography>
+                                <Typography variant="h4" align="center" gutterBottom>Hai votato per la categoria corrente!</Typography>
+                                <Typography variant="h4" align="center"> Attendi l'apertura della prossima categoria o segui l'aggiornamento delle
+                                    classifiche.</Typography>
+                                <Box mt={4} textAlign="center">
+                                    <Button
+                                        fullWidth
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={() => {
+                                            setTriggerFetchCategory(!triggerFetchCategory);
+                                        }}
+                                    >
+                                        Aggiorna
+                                    </Button>
+                                    <Typography mt={2} variant="body1" align="center" gutterBottom>
+                                        Clicca AGGIORNA non appena ti verrà comunicato che è stata aperta una nuova votazione.
+                                    </Typography>
+                                </Box>
                             </>
                         )
                     }
+                    {
+                        noCategory && (
+                            <>
+                                <Typography variant="h4" align="center" gutterBottom>Hey, so che non
+                                    vedi l'ora ma le votazioni non
+                                    sono ancora aperte!</Typography>
+                                {
+                                    new Date() < new Date("2025-05-17T16:00:00Z") && (
+                                        <Typography variant="h4" align="center"> Torna sabato 17 maggio 2025 alle 18:00
+                                            😉</Typography>
+                                    )
+                                }
+                                {
+                                    new Date() > new Date("2025-05-17T16:00:00Z") && (
+                                        <>
+                                            <Box mt={4} textAlign="center">
+                                                <Button
+                                                    fullWidth
+                                                    variant="contained"
+                                                    color="primary"
+                                                    onClick={() => {
+                                                        setTriggerFetchCategory(!triggerFetchCategory);
+                                                    }}
+                                                >
+                                                    Aggiorna
+                                                </Button>
+                                            </Box>
+                                            <Typography mt={2} variant="body1" align="center" gutterBottom>
+                                                Clicca AGGIORNA non appena ti verrà comunicato che sono state aperte le votazioni.
+                                            </Typography>
+                                        </>
+                                    )
+                                }
+                            </>
+                        )
+                    }
+                    {
+                        category && (
+                            <>
+                                <Typography variant="h4" align="center" gutterBottom>{category.label}</Typography>
+
+                                {category.votePoints.map((points, index) => (
+                                    <FormControl key={points} fullWidth margin="normal">
+                                        <InputLabel id={`vote-select-label-${points}`}>Assegna {points} punti a</InputLabel>
+                                        <Select
+                                            labelId={`vote-select-label-${points}`}
+                                            value={votes.find(v => v.points === points)?.country || ""}
+                                            label={`Assegna ${points} punti a`}
+                                            onChange={(e) => {
+                                                const country = e.target.value;
+                                                setVotes(prevVotes => {
+                                                    // Rimuovi country già selezionata altrove o già associata a questo punteggio
+                                                    const filtered = prevVotes.filter(v => v.points !== points && v.country !== country);
+                                                    return [...filtered, { country, points }];
+                                                });
+                                            }}
+                                        >
+                                            <MenuItem value="">
+                                                <em>-- Seleziona un paese --</em>
+                                            </MenuItem>
+                                            {availableCountries
+                                                .filter(c => !votes.find(v => v.country === c.name && v.points !== points))
+                                                .map(c => (
+                                                    <MenuItem key={c.name} value={c.name}>
+                                                        {getFlagEmoji(c.isoCode)} {c.label}{(!!c.participant || !!c.assignedGuest) && " - "}{c.participant ? c.participant : c.assignedGuest}
+                                                    </MenuItem>
+                                                ))}
+                                        </Select>
+                                    </FormControl>
+                                ))}
+
+                                <Box mt={4} textAlign="center">
+                                    <Button
+                                        fullWidth
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={onSubmit}
+                                        disabled={!isVoteValid()}
+                                    >
+                                        Invia voti
+                                    </Button>
+                                </Box>
+                            </>
+                        )
+                    }
+                    <Box mt={2} textAlign="center">
+                        {error && <Alert severity="error" sx={{mb: 2}}>{error}</Alert>}
+                        {
+                            successMessage &&
+                            <Snackbar open={snackbarOpen} autoHideDuration={3000}
+                                      onClose={() => setSnackbarOpen(false)}>
+                                <Alert severity="success" sx={{ width: '100%' }}>{successMessage}</Alert>
+                            </Snackbar>
+                        }
+                    </Box>
                 </Box>
             </Container>
         </Box>
